247c9ffddd7acdb80bc5da3b2c21e7b6516b58f7
sandangel <vinhsannguyen91@gmail.com>
2018-02-19 01:46:29 +0900

    test(store): use jasmine for testing modules

---
+++ tests.js
@@ -0,0 +1,29 @@
+const { join } = require('path');
+
+require('ts-node').register({ project: join(__dirname, './tsconfig.spec.json') });
+require('core-js/es7/reflect');
+require('zone.js/dist/zone-node.js');
+require('zone.js/dist/long-stack-trace-zone.js');
+require('zone.js/dist/proxy.js');
+require('zone.js/dist/sync-test.js');
+require('zone.js/dist/async-test.js');
+require('zone.js/dist/fake-async-test.js');
+
+const Jasmine = require('jasmine');
+
+const runner = new Jasmine();
+
+global.jasmine = runner.jasmine;
+require('zone.js/dist/jasmine-patch.js');
+
+const { getTestBed } = require('@angular/core/testing');
+const { ServerTestingModule, platformServerTesting } = require('@angular/platform-server/testing');
+
+getTestBed().initTestEnvironment(ServerTestingModule, platformServerTesting());
+
+runner.loadConfig({
+  spec_dir: 'modules',
+  spec_files: ['**/*.spec.ts']
+});
+
+runner.execute();
