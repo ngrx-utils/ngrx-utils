605d42a8b0c22ae8a698f4dbdeade7fc1f911cca
sandangel <vinhsannguyen91@gmail.com>
2018-03-21 12:57:55 +0900

    test: use karma + jasmine as test runner

--- build-config.js
+++ build-config.js
@@ -13,22 +13,23 @@ const buildVersion = package.version;
  * Required Angular version for all Angular Material packages. This version will be used
  * as the peer dependency version for Angular in all release packages.
  */
-const angularVersion = '>=6.0.0-beta.0 <7.0.0';
+const angularVersion = '>=5.0.0 <7.0.0';
 
 /** License that will be placed inside of all created bundles. */
 const buildLicense = `/**
  * @license
- * Copyright Google LLC All Rights Reserved.
- *
+ * MIT License
+ * Copyright (c) 2018 San Nguyen
+ * 
  * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
+ * found in the LICENSE file at https://github.com/ngrx-utils/ngrx-utils/blob/master/LICENSE
  */`;
 
 module.exports = {
   projectVersion: buildVersion,
   angularVersion: angularVersion,
   projectDir: __dirname,
-  packagesDir: join(__dirname, 'src'),
+  packagesDir: join(__dirname, 'libs'),
   outputDir: join(__dirname, 'dist'),
   licenseBanner: buildLicense
 };
