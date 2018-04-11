2b8a122240aa5ae3dc265dbcd337fbaa885581aa
sandangel <vinhsannguyen91@gmail.com>
2018-04-11 10:33:10 +0900

    build: refactor gulp tasks

--- tools/gulp/gulpfile.ts
+++ tools/gulp/gulpfile.ts
@@ -4,7 +4,7 @@ import { replaceVersion, buildAll } from './tasks/builds';
 import { coverageGenerate, coverageUpload } from './tasks/coverage';
 import { help } from './tasks/default';
 import { lint } from './tasks/lint';
-import { testLibs } from './tasks/unit-test';
+import { testUnit } from './tasks/unit-test';
 import { validateRelease } from './tasks/validate-release';
 import { npmWhoAmI, npmLogout, npmPublish } from './tasks/publish';
 import { deployGithubBuilds } from './tasks/deploy-github';
@@ -13,40 +13,48 @@ task('clean:dist', cleanDist);
 task('replace-version', replaceVersion);
 task('build:all', buildAll);
 
+const buildRelease = series('clean:dist', 'build:all', 'replace-version');
 /** Builds release packages */
-task('build:release', series('clean:dist', 'build:all', 'replace-version'));
+task('build:release', buildRelease);
 
 /** Test coverage */
 task('coverage:clean', cleanCoverage);
 task('coverage:generate', coverageGenerate);
 task('coverage:upload', coverageUpload);
-task('coverage', series('coverage:clean', 'coverage:generate', 'coverage:upload'));
+const coverage = series('coverage:clean', 'coverage:generate', 'coverage:upload');
+task('coverage', coverage);
 
 task('help', help);
-task('default', series('help'));
+task('default', help);
 
 task('deploy:github-builds', deployGithubBuilds);
-task('deploy', series('build:all', 'deploy:github-builds'));
+const deploy = series('build:release', 'deploy:github-builds');
+task('deploy', deploy);
 
 task('lint', lint);
 
-task('test:unit', testLibs);
-task('test', parallel('test:unit'));
+task('test:unit', testUnit);
+const test = parallel('test:unit');
+task('test', test);
 
 task('ci:lint', lint);
-task('ci:test', testLibs);
-task('ci:build', buildAll);
+task('ci:test', testUnit);
+task('ci:build', buildRelease);
 /** Task that uploads the coverage results to codecov. */
-task('ci:coverage', series('coverage'));
-
-task('ci', series('ci:build', parallel('ci:lint', 'ci:test'), 'ci:coverage'));
+task('ci:coverage', coverage);
+const ci = series('ci:build', parallel('ci:lint', 'ci:test'), 'ci:coverage');
+task('ci', ci);
 
 /** Make sure we're logged in. */
 task('npm:whoami', npmWhoAmI);
 task('npm:logout', npmLogout);
 task('npm:publish', npmPublish);
 task('validate-release', validateRelease);
-task(
-  'publish',
-  series('npm:whoami', 'build:release', 'validate-release', 'npm:publish', 'npm:logout')
+const publish = series(
+  'npm:whoami',
+  'build:release',
+  'validate-release',
+  'npm:publish',
+  'npm:logout'
 );
+task('publish', publish);
--- tools/gulp/tasks/unit-test.ts
+++ tools/gulp/tasks/unit-test.ts
@@ -8,7 +8,7 @@ const { releasePackages } = buildConfig;
  * Runs the unit tests. Does not watch for changes.
  * This task should be used when running tests on the CI server.
  */
-export const testLibs = parallel(
+export const testUnit = parallel(
   ...releasePackages.map(pkg =>
     execNodeTask('@angular/cli', 'ng', ['test', pkg, '--code-coverage', '--progress', 'false'], {
       failOnStderr: false
