4d0e733895ae2fa5123882eb436d9c1ed7938469
sandangel <vinhsannguyen91@gmail.com>
2018-05-10 07:49:49 +0900

    feat(schematics): add enum and type alias

--- projects/schematics/action-helper/index.ts
+++ projects/schematics/action-helper/index.ts
@@ -9,15 +9,18 @@ export type ActionHelperOptions = {
 };
 
 type ActionMetadata = {
-  feature: string;
+  featureName: string;
   eventSource: string;
   actionType: string;
   className: string;
+  actionFullText: string;
 };
 
 export default function(options: ActionHelperOptions): Rule {
   return (host: Tree, context: SchematicContext) => {
+    // TODO: Handle path glob
     const path = options.path;
+
     const text = host.read(path);
     if (text === null) {
       throw new SchematicsException(`File ${path} does not exist.`);
@@ -39,11 +42,27 @@ export default function(options: ActionHelperOptions): Rule {
       return false;
     });
 
-    const actionMetadata: ActionMetadata[] = [];
+    /**
+     * Remove all declared Enum, Type Alias and use the new output
+     * for shake of simplicity.
+     */
+    // TODO: Lookup output Enum and Type Alias to see whether these should be removed
+    const actionTypeEnum = statements.filter(ts.isEnumDeclaration);
+    const actionTypeAlias = statements.filter(ts.isTypeAliasDeclaration);
+
+    // TODO: Move outside for multi action declaration files
     const actionHashTable: { [action: string]: string } = {};
 
+    const featureMap: { [feature: string]: ActionMetadata[] } = {};
+
     actionClasses.forEach(cls => {
-      const classEl = cls.members.filter((m): m is ts.PropertyDeclaration => {
+      if (cls.name == null) {
+        throw new SchematicsException(
+          `You MUST specify the name of action class: \n` + `${cls.getText()}`
+        );
+      }
+
+      const actionTypeProp = cls.members.filter((m): m is ts.PropertyDeclaration => {
         if (ts.isPropertyDeclaration(m)) {
           return m.name.getText() === 'type';
         }
@@ -51,45 +70,83 @@ export default function(options: ActionHelperOptions): Rule {
         return false;
       })[0];
 
-      if (cls.name == null) {
-        throw new SchematicsException(
-          `You MUST specify the name of action class: \n` + `${cls.getText()}`
-        );
-      }
+      const className = cls.name.text;
 
-      const className = cls.name.getText();
-
-      if (classEl.initializer == null) {
+      if (actionTypeProp.initializer == null) {
         throw new SchematicsException(
           `You MUST initialize the action type of class '${className}'!`
         );
       }
 
-      const action = classEl.initializer.getText().replace(/'/g, '');
+      const actionFullText = actionTypeProp.initializer.getText().replace(/'/g, '');
 
-      if (actionHashTable[action] !== undefined) {
+      if (actionHashTable[actionFullText] !== undefined) {
         throw new SchematicsException(
-          `Duplicated '${action}' in '${className}' and '${actionHashTable[action]}'`
+          `Duplicated '${actionFullText}' in '${className}' and '${
+            actionHashTable[actionFullText]
+          }'`
         );
       }
 
       const actionTypeRegex = new RegExp(/\[(.+)\.(.+)\](.+)/);
-      const result = actionTypeRegex.exec(action);
+      const actionTypeNoSourceRegex = new RegExp(/\[(.+)(.*)\](.+)/);
+      const result =
+        actionTypeRegex.exec(actionFullText) || actionTypeNoSourceRegex.exec(actionFullText);
 
       if (result == null) {
-        throw new SchematicsException(`Could not parse action type '${action}'`);
+        throw new SchematicsException(
+          `Could not parse action type '${actionFullText}' \n` +
+            `You should follow action type convention: '[Feature.EventSource] Interaction Event'`
+        );
       }
 
-      actionHashTable[action] = className;
-      actionMetadata.push({
+      actionHashTable[actionFullText] = className;
+
+      const featureName = result[1].replace(/\s/g, '');
+
+      if (featureMap[featureName] === undefined) {
+        featureMap[featureName] = [];
+      }
+
+      featureMap[featureName].push({
         className,
-        feature: result[1].replace(/\s/g, ''),
+        featureName,
+        actionFullText,
         eventSource: result[2].replace(/\s/g, ''),
         actionType: result[3].replace(/\s/g, '')
       });
     });
 
-    console.log(actionMetadata);
+    const outputs = Object.keys(featureMap).map(featureName => {
+      const typeAliasDeclaration = ts.createTypeAliasDeclaration(
+        undefined,
+        [ts.createToken(ts.SyntaxKind.ExportKeyword)],
+        `${featureName}ActionUnion`,
+        undefined,
+        ts.createUnionTypeNode(
+          featureMap[featureName].map(m => ts.createTypeReferenceNode(m.className, undefined))
+        )
+      );
+
+      const enumDeclaration = ts.createEnumDeclaration(
+        undefined,
+        [ts.createToken(ts.SyntaxKind.ExportKeyword)],
+        `${featureName}ActionType`,
+        featureMap[featureName].map(m =>
+          ts.createEnumMember(`${m.eventSource}${m.actionType}`, ts.createLiteral(m.actionFullText))
+        )
+      );
+
+      const actionGroup = actionClasses.filter(cls =>
+        featureMap[featureName].some(f => f.className === cls.name!.getText())
+      );
+
+      return {
+        actionEnum: enumDeclaration,
+        actionTypeAlias: typeAliasDeclaration,
+        actionGroup
+      };
+    });
 
     return host;
   };
--- test.actions.ts
+++ test.actions.ts
@@ -1,11 +1,15 @@
 import { Action } from '@ngrx/store';
 
-export default class implements Action {
+export class MyAction implements Action {
   readonly type = '[Feature.HomePage] Do My Action';
   constructor(public payload: any) {}
 }
 
 export class MyAction2 implements Action {
   readonly type = '[Feature.HomePage] Do My Action 2';
+  constructor(public payload: any) {}
+}
+export class MyAction3 implements Action {
+  readonly type = '[Feature] Do My Action 3';
   constructor(public payload: any) {}
 }
