d469f9d8687088e8bc0c747bfecd1637c50dd080
sandangel <vinhsannguyen91@gmail.com>
2018-06-08 07:40:24 +0900

    [WIP] fix: router link active does not update after links change

--- projects/store/src/directives/routerLinkMatch.ts
+++ projects/store/src/directives/routerLinkMatch.ts
@@ -1,88 +1,113 @@
 import {
+  AfterContentInit,
+  ContentChildren,
   Directive,
   ElementRef,
   Input,
   NgModule,
   OnChanges,
   OnDestroy,
-  Renderer2,
-  SimpleChanges
+  QueryList,
+  Renderer2
 } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
-import { combineLatest, Subject } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
-
+import { NavigationEnd, Router, RouterLink, RouterLinkWithHref } from '@angular/router';
 import { untilDestroy } from '../operators';
 
 export interface MatchExp {
   [classes: string]: string;
 }
 
+/**
+ * TODO: Add docs
+ */
 @Directive({
   selector: '[routerLinkMatch]'
 })
-export class RouterLinkMatch implements OnDestroy, OnChanges {
+export class RouterLinkMatch implements OnDestroy, OnChanges, AfterContentInit {
   private _curRoute: string;
   private _matchExp: MatchExp;
-  private _onChangesHook = new Subject<MatchExp>();
+
+  @ContentChildren(RouterLink, { descendants: true })
+  links: QueryList<RouterLink>;
+  @ContentChildren(RouterLinkWithHref, { descendants: true })
+  linksWithHrefs: QueryList<RouterLinkWithHref>;
 
   @Input('routerLinkMatch')
-  set routerLinkMatch(v: MatchExp) {
-    if (v && typeof v === 'object') {
-      this._matchExp = v;
+  set routerLinkMatch(matchExp: MatchExp) {
+    if (matchExp && typeof matchExp === 'object') {
+      this._matchExp = matchExp;
     } else {
       throw new TypeError(
-        `Unexpected type '${typeof v}' of value for ` +
+        `Unexpected type '${typeof matchExp}' of value for ` +
           `input of routerLinkMatch directive, expected 'object'`
       );
     }
   }
 
-  constructor(router: Router, private _renderer: Renderer2, private _ngEl: ElementRef) {
-    combineLatest(router.events, this._onChangesHook)
-      .pipe(map(([e]) => e), filter(e => e instanceof NavigationEnd), untilDestroy(this))
-      .subscribe(e => {
+  constructor(private router: Router, private _renderer: Renderer2, private _ngEl: ElementRef) {
+    router.events.pipe(untilDestroy(this)).subscribe(e => {
+      if (e instanceof NavigationEnd) {
         this._curRoute = (e as NavigationEnd).urlAfterRedirects;
-
-        this._updateClass(this._matchExp);
-      });
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes['routerLinkMatch']) {
-      this._onChangesHook.next(changes['routerLinkMatch'].currentValue);
-    }
-  }
-
-  private _updateClass(v: MatchExp): void {
-    Object.keys(v).forEach(cls => {
-      if (v[cls] && typeof v[cls] === 'string') {
-        const regexp = new RegExp(v[cls]);
-        if (this._curRoute.match(regexp)) {
-          this._toggleClass(cls, true);
-        } else {
-          this._toggleClass(cls, false);
-        }
-      } else {
-        throw new TypeError(
-          `Could not convert match value to Regular Expression. ` +
-            `Unexpected type '${typeof v[cls]}' for value of key '${cls}' ` +
-            `in routerLinkMatch directive match expression, expected 'non-empty string'`
-        );
+        this._update();
       }
+    });
+  }
+
+  ngOnChanges() {
+    this._update();
+  }
+
+  ngAfterContentInit() {
+    this.links.changes.pipe(untilDestroy(this)).subscribe(() => this._update());
+    this.linksWithHrefs.changes.pipe(untilDestroy(this)).subscribe(() => this._update());
+    this._update();
+  }
+
+  private _update(): void {
+    if (!this.links || !this.linksWithHrefs || !this.router.navigated) {
+      return;
+    }
+
+    /**
+     * This will push update to the last of event queue to avoid
+     * active class won't apply when list of link has changed.
+     */
+    Promise.resolve().then(() => {
+      const matchExp = this._matchExp;
+
+      Object.keys(matchExp).forEach(cls => {
+        if (matchExp[cls] && typeof matchExp[cls] === 'string') {
+          const regexp = new RegExp(matchExp[cls]);
+          if (this._curRoute.match(regexp)) {
+            this._toggleClass(cls, true);
+          } else {
+            this._toggleClass(cls, false);
+          }
+        } else {
+          throw new TypeError(
+            `Could not convert match value to Regular Expression. ` +
+              `Unexpected type '${typeof matchExp[cls]}' for value of key '${cls}' ` +
+              `in routerLinkMatch directive match expression, expected 'non-empty string'`
+          );
+        }
+      });
     });
   }
 
   private _toggleClass(classes: string, enabled: boolean): void {
-    classes = classes.trim();
-
-    classes.split(/\s+/g).forEach(cls => {
-      if (enabled) {
-        this._renderer.addClass(this._ngEl.nativeElement, cls);
-      } else {
-        this._renderer.removeClass(this._ngEl.nativeElement, cls);
-      }
-    });
+    /**
+     * FIXME: Refine active class to apply (store in private variable).
+     */
+    classes
+      .trim()
+      .split(/\s+/g)
+      .forEach(cls => {
+        if (enabled) {
+          this._renderer.addClass(this._ngEl.nativeElement, cls);
+        } else {
+          this._renderer.removeClass(this._ngEl.nativeElement, cls);
+        }
+      });
   }
 
   ngOnDestroy() {}
